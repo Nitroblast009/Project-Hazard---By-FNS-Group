2fd6b6586c6906bd2e7052dc696d13220763310f
ND-11HJ-696424 <114433540+Kiki8181@users.noreply.github.com>
2022-10-17 16:26:56 +0000

    Added colour file

---
+++ colours.py
@@ -0,0 +1,2 @@
+landColour = (20, 173, 0)
+mainScreenColour = (48, 141, 255)
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import pygame, sys
+import pygame
+from colours import *
 from pygame.locals import QUIT
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('Project Hazard!')
-while True:
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption('Project Hazard')
+screen.fill(mainScreenColour)
+
+
+running = True
+while running:
+    pygame.draw.rect(screen, landColour, pygame.Rect(30, 30, 140, 300))
+    pygame.draw.rect(screen, landColour, pygame.Rect(180, 30, 140, 300))
+    pygame.draw.rect(screen, landColour, pygame.Rect(330, 30, 140, 300))
+    pygame.draw.rect(screen, landColour, pygame.Rect(480, 30, 140, 300))
+
+    
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
+            break
     pygame.display.update()
+
+pygame.quit()
