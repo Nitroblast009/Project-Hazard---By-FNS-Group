a1c5cdc7f47b6925ad01f13ef023c7a65d025345
Faizaan A <57153315+Nitroblast009@users.noreply.github.com>
2022-10-17 15:46:05 +0000

    removed superfluities

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ import pygame, sys
 from pygame.locals import QUIT
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
+DISPLAYSURF = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Project Hazard!')
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
-
-
-print ("epic")
-print ("cool")
-print("Hello.")
