e551d3ba282e7b090fda5c1b919d7cb4d9b2efc9
Faizoo <faizaan3424@gmail.com>
2022-11-06 17:14:57 -0500

    implemented all north american territories

--- game_board.py
+++ game_board.py
@@ -13,9 +13,57 @@ def makeGameBoard(map=None):
         pygame.draw.rect(gameBoard, "black", (0, i * 100, 700, 1))
 
     if map == "All" or map == "Americas":
+        Greenland.draw(gameBoard)
+
         Nuvuk.draw(gameBoard)
+        Alaska.draw(gameBoard)
+        Northwest.draw(gameBoard)
+        BritishColumbia.draw(gameBoard)
+        Nunavut.draw(gameBoard)
+        Alberta.draw(gameBoard)
+        Ontario.draw(gameBoard)
+        Newfoundland.draw(gameBoard)
+        Quebec.draw(gameBoard)
+        PEI.draw(gameBoard)
+
+        Mexico.draw(gameBoard)
+        Caribbeans.draw(gameBoard)
+
+        Ecuador.draw(gameBoard)
+        Columbia.draw(gameBoard)
+        Venezuela.draw(gameBoard)
+        Peru.draw(gameBoard)
+        Bolivia.draw(gameBoard)
+        Brazil.draw(gameBoard)
+        Chile.draw(gameBoard)
+        Argentina.draw(gameBoard)
 
     if map == "All" or map == "Afroeurasia":
-        pass
+        UK.draw(gameBoard)
+        France.draw(gameBoard)
+        Germany.draw(gameBoard)
+        Scandinavia.draw(gameBoard)
+        Ukraine.draw(gameBoard)
+
+        Turkey.draw(gameBoard)
+        Egypt.draw(gameBoard)
+        Nigeria.draw(gameBoard)
+        Ethiopia.draw(gameBoard)
+        SaudiArabia.draw(gameBoard)
+        Madagascar.draw(gameBoard)
+
+        Siberia.draw(gameBoard)
+        FarEast.draw(gameBoard)
+        Kazakhstan.draw(gameBoard)
+        Mongolia.draw(gameBoard)
+        China.draw(gameBoard)
+        Korea.draw(gameBoard)
+        India.draw(gameBoard)
+        Japan.draw(gameBoard)
+
+        Indonesia.draw(gameBoard)
+        Philippines.draw(gameBoard)
+        Australia.draw(gameBoard)
+        NewZealand.draw(gameBoard)
 
     return gameBoard
