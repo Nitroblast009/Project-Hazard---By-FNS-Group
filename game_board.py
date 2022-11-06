c91223a3f43a798ccbcf8a6cf3ceda71d6f7aa75
Faizoo <faizaan3424@gmail.com>
2022-11-06 16:40:31 -0500

    added gridlines

--- game_board.py
+++ game_board.py
@@ -4,7 +4,13 @@ from constants import *
 
 def makeGameBoard(map=None):
     gameBoard = pygame.Surface((700, 600))
-    gameBoard.fill(white)
+    gameBoard.fill("white")
+
+    for i in range(7):
+        pygame.draw.rect(gameBoard, "black", (i * 100, 0, 1, 600))
+
+    for i in range(6):
+        pygame.draw.rect(gameBoard, "black", (0, i * 100, 700, 1))
 
     if map == "All" or map == "Americas":
         Nuvuk.draw(gameBoard)
