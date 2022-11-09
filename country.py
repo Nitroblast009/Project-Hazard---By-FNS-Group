477a342deffb0ae08d97516af1fb157460de16a2
Faizaan A <57153315+Nitroblast009@users.noreply.github.com>
2022-11-09 16:56:22 +0000

    fixed colour schema for selected territories

--- country.py
+++ country.py
@@ -25,10 +25,16 @@ class Country:
         self.player = player
 
     def draw(self, surface):
-      pygame.gfxdraw.filled_polygon(surface, self.coords, pygame.Color("red") if self.player == "p1" else pygame.Color("0x0271ad"))
+      #pygame.gfxdraw.filled_polygon(surface, self.coords, "red" if self.player == "p1" else pygame.Color("0x0271ad"))
+      if self.player == "p1":
+        colour = pygame.Color("0xb51818") if self.selected else pygame.Color("red")
+      elif self.player == "p2":
+        colour = pygame.Color("0x014080") if self.selected else pygame.Color("0x0271ad")
+
+      pygame.gfxdraw.filled_polygon(surface, self.coords, colour)
       
       pygame.draw.polygon(
-            surface, "black", self.coords, width=6 if self.selected else 4)
+            surface, "black", self.coords, width=5 if self.selected else 4)
 
     def check(self, mouseCoord):
         global selectedCountry
